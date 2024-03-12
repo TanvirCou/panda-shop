f86f29a7f9bea4c7770f508228d7957399f497ac
TanvirCoU <ahmeadtanvir@stud.cou.ac.bd>
2024-03-13 02:55:56 +0600

    responsive issue fixed

--- src/components/Shared/Header/Header.jsx
+++ src/components/Shared/Header/Header.jsx
@@ -148,7 +148,7 @@ const Header = ({ activeHeading }) => {
                         <BiMenuAltLeft size={35} className="cursor-pointer" onClick={() => setMobileSidebar(true)} />
                     </div>
                     <Link to="/">
-                        <p className="text-2xl font-bold font-[Poppins] cursor-pointer">Panda-Shop</p>
+                        <p className="text-3xl font-semibold font-[Poppins] cursor-pointer text-gray-800">Panda-Shop</p>
                     </Link>
                     <div className="relative px-3 cursor-pointer" onClick={() => setCartOpen(!cartOpen)}>
                         <IoCartOutline size={30} className="text-black" />
